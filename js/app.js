1383be708e41c34927954198152cb1674d1fa055
shorifuddin2 <shorifuddin240915@gmail.com>
2022-03-10 22:19:13 +0600

    profile picture updete is show

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-    return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+    return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
@@ -51,18 +51,21 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
+  console.log(post)
     const image = post.image;
+    const img = post.userImage;
+    // console.log(image)
     const div = document.createElement( "article" );
     div.classList.add( "post" );
     div.innerHTML = `
               <div class="post__header">
                 <div class="post__profile">
                   <a
-                    href="https://github.com/ProgrammingHero1"
+                    href=""https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80""
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${img}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
